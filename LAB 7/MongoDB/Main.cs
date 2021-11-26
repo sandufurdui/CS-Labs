ab5f8126d582a3f01c9f3aa1484df6ef198b15cc
sandufurdui <76735601+sandufurdui@users.noreply.github.com>
2021-11-26 02:54:57 +0200

    l

---
+++ LAB 7/MongoDB/Main.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MongoDB
+{
+    class Program
+    {
+        public static string randomWord = "";
+        static void Main(string[] args)
+        {
+            MongoClient dbClient = new MongoClient("mongodb://furdui:Password1@cluster0-shard-00-00.184so.mongodb.net:27017,cluster0-shard-00-01.184so.mongodb.net:27017,cluster0-shard-00-02.184so.mongodb.net:27017/MongoDB?ssl=true&replicaSet=atlas-opxi80-shard-0&authSource=admin&retryWrites=true&w=majority");
+            var database = dbClient.GetDatabase("MongoDB");
+
+            var collection = database.GetCollection<BsonDocument>("MongoDB");
+
+            var document = new BsonDocument();
+
+
+            BsonDocument firstStudent =
+        new BsonDocument { { "University", "UTM" }, { "fullName", "Name Surname" }, { "email", " test1@gmail.com" }, { "dateBirth", "01/01/2000" }, { "sudentGroup", "FAF-192" } }
+             ;
+
+
+            BsonDocument secondStudent =
+        new BsonDocument { { "University", "UTM" }, { "fullName", "Name Surname" }, { "email", " test2@gmail.com" }, { "dateBirth", "01/01/2000" }, { "sudentGroup", "FAF-192" } }
+             ;
+
+
+            BsonDocument thirdStudent =
+        new BsonDocument { { "University", "UTM" }, { "fullName", "Name Surname" }, { "email", " test3@gmail.com" }, { "dateBirth", "01/01/2000" }, { "sudentGroup", "FAF-192" } }
+             ;
+
+
+            database.DropCollection("students");
+
+            collection.InsertMany(new List<BsonDocument> { firstStudent, secondStudent, thirdStudent });
+
+            Console.WriteLine(document.ToString());
+            var filter = Builders<BsonDocument>.Filter.Eq("University", "UTM");
+            var result = collection.Find(filter).ToList();
+
+            MongoDBController.updateDBWithEncryptedValue("dateBirth", result, collection);
+
+            //MongoDBController.updateDBWithDecryptedValue("dateBirth", result, collection);
+
+        }
+    }
+
+}
+
+
+
+
+
---
+++ LAB 7/MongoDB/MongoDBController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDB
+{
+    public class MongoDBController
+    {
+        public static string encryptedString2 = "";
+        public static void updateDBWithEncryptedValue(string dbField, List<BsonDocument> filtersResult, IMongoCollection<BsonDocument> collection)
+        {
+
+            foreach (var doc in filtersResult)
+            {
+
+
+                var value = doc.GetValue(dbField);
+
+                var thisFilter = Builders<BsonDocument>.Filter.Eq("_id", doc.GetValue("_id"));
+
+                Console.WriteLine("Value: " + value.ToString());
+                Console.WriteLine("\n------\n");
+                string encryptedString = Encrypt(value.ToString());
+                encryptedString2 = Encrypt(encryptedString);
+                Console.WriteLine("ENCRYPTED STRING: " + encryptedString2);
+                Console.WriteLine("\n------\n");
+
+                var update = Builders<BsonDocument>.Update.Set(dbField, encryptedString2);
+
+                collection.UpdateOne(thisFilter, update);
+            }
+        }
+
+        public static void updateDBWithDecryptedValue(string dbField, List<BsonDocument> filtersResult, IMongoCollection<BsonDocument> collection)
+        {
+
+            foreach (var doc in filtersResult)
+            {
+                var value = doc.GetValue(dbField);
+
+                var thisFilter = Builders<BsonDocument>.Filter.Eq("_id", doc.GetValue("_id"));
+
+                string decryptedString = Decrypt(encryptedString2);
+                string decryptedString2 = Decrypt(decryptedString);
+                Console.WriteLine("DECRYPTED STRING: " + decryptedString2);
+                Console.WriteLine("\n------\n");
+                var update = Builders<BsonDocument>.Update.Set(dbField, decryptedString2);
+
+                collection.UpdateOne(thisFilter, update);
+            }
+        }
+
+        public static string Encrypt(string text)
+        {
+            string EncryptionKey = "EncryptionKey";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(text);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    text = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return text;
+        }
+
+        public static string Decrypt(string text)
+        {
+            string EncryptionKey = "EncryptionKey";
+            text = text.Replace(" ", "+");
+            byte[] cipherBytes = Convert.FromBase64String(text);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    text = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return text;
+        }
+    }
+}
